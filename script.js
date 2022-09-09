f609038af632df8f3c17223ffe2f7fd3dde60d72
vgn-612 <109751451+vgn-612@users.noreply.github.com>
2022-09-09 12:58:14 +0300

    pridetos linksmybes

--- script.js
+++ script.js
@@ -1,11 +1,68 @@
-document.getElementById("mygtukas").addEventListener("click", vykdom);
+document.getElementById("mygtukas").addEventListener("click", vykdom); //stebim mygtuko bukle ir vykdom()
+document.getElementById("number").addEventListener("keypress", mygtukas); //linksmybem ENTER mygtuko klausymas
+let kvadratuSkaicius = 0;
+let alertSkaicius = 0;
 
 function vykdom() {
-  let a = document.getElementById("number").value;
-  if (a) {
-    for (let i = a; i > 0; i--) {
-      
+  //restartuojam jei iveda nauja reiksme (istrinam "bendra" div)
+  if (kvadratuSkaicius != 0) {
+    document.getElementById("bendras").innerHTML = "";
+    kvadratuSkaicius = 0;
+  }
+  //sukuriam kvadratelius
+  if (kvadratuSkaicius == 0) {
+    kvadratuSkaicius = document.getElementById("number").value;
+    if (kvadratuSkaicius) {
+      if (kvadratuSkaicius < 20) {
+        for (let i = kvadratuSkaicius; i > 0; i--) {
+          const deze = document.createElement("div");
+          deze.innerHTML = "*";
+          document.getElementById("bendras").appendChild(deze);
+        }
+      } else {
+        alert(
+          "...taigi buvo " + '"' + "prašoma" + '"' + " įvesti mažiau nei 20 ..."
+        );
+      }
+    } else {
+      alert(
+        "Įvesties klaida\nVis dėlto kažkaip reikėtų spręsti šią problemą... "
+      );
     }
-    a = 0;
+  }
+}
+//linksmybes
+function mygtukas(event) {
+  if (event.key === "Enter") {
+    switch (alertSkaicius) {
+      case 0:
+        event.preventDefault();
+        document.getElementById("mygtukas").click();
+        alert(
+          "ką aš sakiau?\nSPAUDŽIAM MYGTUKĄ, o ne enter ...\n...nu bet šį kartą paspausiu už tave...click..."
+        );
+        alertSkaicius++;
+        break;
+      case 1:
+        event.preventDefault();
+        document.getElementById("mygtukas").click();
+        alert(
+          "rimtai? vis dar enter? \ntai gal mygtuką pavadint enter?...\n...click..."
+        );
+        document.getElementById("mygtukas").innerHTML = "ENTER";
+        alertSkaicius++;
+        break;
+      case 2:
+        event.preventDefault();
+        document.getElementById("mygtukas").click();
+        alert("$#%^%%$#\n...click...");
+        alertSkaicius++;
+        break;
+      default:
+        alert(
+          "nu kaip sakoma :\nko neišmokai - tai neprilaižysi\nstengemės padėt išmokt... daugiau už tave nespaudinėsim!"
+        );
+        break;
+    }
   }
 }
